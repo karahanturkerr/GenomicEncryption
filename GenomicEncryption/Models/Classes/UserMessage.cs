4aa6d56e92fbc83ad5efc3722ca00122ec1f185a
ygnibrahim <thedown172@gmail.com>
2024-01-09 18:20:27 +0300

    add-message-entity

---
+++ GenomicEncryption/Models/Classes/UserMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GenomicEncryption.Models.Classes
+{
+    public class UserMessage
+    {
+        public int UserId { get; set; }
+        public int MessageId { get; set; }
+    }
+}

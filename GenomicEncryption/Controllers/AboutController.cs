06e37411bd9ac1de02d8f30893b934a46d110dc9
ygnibrahim <thedown172@gmail.com>
2024-01-09 18:16:48 +0300

    add-about.

---
+++ GenomicEncryption/Controllers/AboutController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GenomicEncryption.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Team()
+        {
+            return View();
+        }
+        public ActionResult School()
+        {
+            return View();
+        }
+        public ActionResult Teacher()
+        {
+            return View();
+        }
+
+    }
+}
--- GenomicEncryption/Controllers/EncryptionController.cs
+++ GenomicEncryption/Controllers/EncryptionController.cs
@@ -1,45 +1,45 @@
-﻿using GenomicEncryption.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿//using GenomicEncryption.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
 
-namespace GenomicEncryption.Controllers
-{
-    public class EncryptionController : Controller
-    {
-        [HttpPost]
-        public JsonResult IndexEncrypt(EncryptViewMoel model)
-        {
-            AesEncryption aes = new AesEncryption();
-            var result = aes.Encrypt(model);
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-        [HttpPost]
-        public JsonResult IndexDecrypt(EncryptViewMoel model)
-        {
-            AesEncryption aes = new AesEncryption();
-            var result = aes.Decrypt(model);
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
+//namespace GenomicEncryption.Controllers
+//{
+//    public class EncryptionController : Controller
+//    {
+//        [HttpPost]
+//        public JsonResult IndexEncrypt(EncryptViewMoel model)
+//        {
+//            AesEncryption aes = new AesEncryption();
+//            var result = aes.Encrypt(model);
+//            return Json(result, JsonRequestBehavior.AllowGet);
+//        }
+//        [HttpPost]
+//        public JsonResult IndexDecrypt(EncryptViewMoel model)
+//        {
+//            AesEncryption aes = new AesEncryption();
+//            var result = aes.Decrypt(model);
+//            return Json(result, JsonRequestBehavior.AllowGet);
+//        }
 
-        [HttpPost]
-        public JsonResult IndexEncryptBurrows(EncryptViewMoel model)
-        {
-            BurrowsWheelerEncyryption brw = new BurrowsWheelerEncyryption();
-            var result = brw.Encrypt(model);
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
-        [HttpPost]
-        public JsonResult IndexDecryptBurrows(EncryptViewMoel model)
-        {
-            BurrowsWheelerEncyryption brw = new BurrowsWheelerEncyryption();
-            var result = brw.Decrypt(model);
-            return Json(result, JsonRequestBehavior.AllowGet);
-        }
+//        [HttpPost]
+//        public JsonResult IndexEncryptBurrows(EncryptViewMoel model)
+//        {
+//            BurrowsWheelerEncyryption brw = new BurrowsWheelerEncyryption();
+//            var result = brw.Encrypt(model);
+//            return Json(result, JsonRequestBehavior.AllowGet);
+//        }
+//        [HttpPost]
+//        public JsonResult IndexDecryptBurrows(EncryptViewMoel model)
+//        {
+//            BurrowsWheelerEncyryption brw = new BurrowsWheelerEncyryption();
+//            var result = brw.Decrypt(model);
+//            return Json(result, JsonRequestBehavior.AllowGet);
+//        }
 
 
 
-    }
-}
+//    }
+//}
